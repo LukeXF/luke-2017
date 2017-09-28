fe5672d18657c42ba3d810280edd66bb454e56c2
Luke Brown <me@luke.sx>
2017-09-28 23:32:08 +0100

    CORS fix for public files

--- app.js
+++ app.js
@@ -33,13 +33,13 @@ app.use(sassMiddleware({
 	indentedSyntax: false, // true = .sass and false = .scss
 	sourceMap: true
 }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
